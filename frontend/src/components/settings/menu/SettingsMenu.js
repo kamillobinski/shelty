0fb8f6100c083fd5f6275293503bbdb3302b5082
kamillobinski <kamil.lobinski@gmail.com>
2020-12-30 04:18:13 +0100

    Change: link to navlink and select active

--- frontend/src/components/settings/menu/SettingsMenu.js
+++ frontend/src/components/settings/menu/SettingsMenu.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./settingsmenu.css";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const SettingsMenu = (props) => {
   return (
@@ -8,37 +8,37 @@ const SettingsMenu = (props) => {
       <div className="settingsMenu-list">
         <div className="settingsMenu-item">
           <span>Account</span>
-          <Link to="/admin/settings/account/edit">
+          <NavLink to="/admin/settings/account/edit" activeClassName='active'>
             <div className="settingsMenu-item-subitem">
               <span>• Edit personal information</span>
             </div>
-          </Link>
+          </NavLink>
         </div>
         <div className="settingsMenu-item">
           <span>Users</span>
-          <Link to="/admin/settings/users/register">
+          <NavLink to="/admin/settings/users/register" activeClassName='active'>
             <div className="settingsMenu-item-subitem">
               <span>• Register new account</span>
             </div>
-          </Link>
-          <Link to="/admin/settings/users/manage">
+          </NavLink>
+          <NavLink to="/admin/settings/users/manage" activeClassName='active'>
             <div className="settingsMenu-item-subitem">
               <span>• Manage accounts</span>
             </div>
-          </Link>
+          </NavLink>
         </div>
         <div className="settingsMenu-item">
           <span>Animal</span>
-          <Link to="/admin/settings/breed/edit">
+          <NavLink to="/admin/settings/breed/edit" activeClassName='active'>
             <div className="settingsMenu-item-subitem">
               <span>• Manage breed</span>
             </div>
-          </Link>
-          <Link to="/admin/settings/species/edit">
+          </NavLink>
+          <NavLink to="/admin/settings/species/edit" activeClassName='active'>
             <div className="settingsMenu-item-subitem">
               <span>• Manage species</span>
             </div>
-          </Link>
+          </NavLink>
         </div>
       </div>
     </div>
