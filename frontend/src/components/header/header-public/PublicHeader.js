2e416a6d5f6483dbe24f39a416620444823a9b8b
kamillobinski <kamil.lobinski@gmail.com>
2021-01-23 01:07:51 +0100

    Update: public header and main menu

--- frontend/src/components/header/header-public/PublicHeader.js
+++ frontend/src/components/header/header-public/PublicHeader.js
@@ -1,36 +1,74 @@
 import React from "react";
-import PrimaryButton from "../../button/PrimaryButton";
 import { Link } from "react-router-dom";
+import PrimaryButton from '../../button/PrimaryButton';
+import { MenuIcon } from '../../../utils/icons/Icons';
 import "./publicheader.css";
 
-const PublicHeader = () => {
-  return (
-    <div className="publicHeader">
-      <div className="publicHeader-inner">
-        <Link to="/">
-          <div className="publicHeader-inner-logo">
-            <span>Shelty</span>
-          </div>
-        </Link>
-        <div className="publicHeader-inner-right-content">
-          <span className="publicHeader-inner-right-content-link">
-            <Link to="/animals">All animals</Link>
-          </span>
-          <span className="publicHeader-inner-right-content-link">
-            <Link to="/finder">Pet finder</Link>
-          </span>
-          <span className="publicHeader-inner-right-content-link">
-            <Link to="/blog">Blog</Link>
-          </span>
-          <span className="publicHeader-inner-right-content-link">About</span>
-          <span className="publicHeader-inner-right-content-link">Contact</span>
-          <Link to="/signin">
-            <PrimaryButton text="SignIn" width="80px" type="dark" />
+const TITLE = "Shelty";
+const MENU = [["/", "Homepage"], ["/animals", "Animals for adoption"], ["/finder", "Find a perfect pet"], ["/blog", "Blog"], ["/signin", "Sign-In"]]
+
+class PublicHeader extends React.Component {
+  constructor() {
+    super();
+    this.state = { shouldDisplayMenu: false }
+    this.toggleMenu = this.toggleMenu.bind(this);
+    this.renderMenu = this.renderMenu.bind(this);
+  }
+
+  renderMenu() {
+    if (this.state.shouldDisplayMenu) {
+      return { visibility: "visible", opacity: 1 }
+    } else {
+      return { visibility: "hidden", opacity: 0 }
+    }
+  }
+
+  toggleMenu() {
+    this.setState({ shouldDisplayMenu: !this.state.shouldDisplayMenu })
+  }
+
+  render() {
+    return (
+      <div className="publicHeader">
+        <div className="publicHeader-logo">
+          <Link to={MENU[0][0]}>
+            <span>{TITLE}</span>
+          </Link>
+        </div>
+        <div className="publicHeader-button" onClick={() => this.toggleMenu()}>
+          <MenuIcon height="30px" fill="black" />
+        </div>
+        <div className="publicHeader-menu" style={this.renderMenu()} >
+          <Link to={MENU[0][0]}>
+            <div className="publicHeader-menu-item">
+              <span>{MENU[0][1]}</span>
+            </div>
+          </Link>
+          <Link to={MENU[1][0]}>
+            <div className="publicHeader-menu-item">
+              <span>{MENU[1][1]}</span>
+            </div>
+          </Link>
+          <Link to={MENU[2][0]}>
+            <div className="publicHeader-menu-item">
+              <span>{MENU[2][1]}</span>
+            </div>
+          </Link>
+          <Link to={MENU[3][0]}>
+            <div className="publicHeader-menu-item">
+              <span>{MENU[3][1]}</span>
+            </div>
+          </Link>
+          <div className="publicHeader-menu-separator"></div>
+          <Link to={MENU[4][0]}>
+            <div className="publicHeader-menu-item">
+              <span>{MENU[4][1]}</span>
+            </div>
           </Link>
         </div>
       </div>
-    </div>
-  );
-};
+    );
+  };
+}
 
 export default PublicHeader;
