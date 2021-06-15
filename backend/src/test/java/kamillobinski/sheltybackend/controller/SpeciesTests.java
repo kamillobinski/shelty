4b992ab0dc3a72e5afee43663058fde91a0cb5e0
kamillobinski <kamil.lobinski@gmail.com>
2021-06-15 20:20:48 +0200

    Update: species controller tests

--- backend/src/test/java/kamillobinski/sheltybackend/controller/SpeciesTests.java
+++ backend/src/test/java/kamillobinski/sheltybackend/controller/SpeciesTests.java
@@ -1,16 +1,16 @@
 package kamillobinski.sheltybackend.controller;
 
+import kamillobinski.sheltybackend.repository.SpeciesRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.test.context.support.WithMockUser;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
@@ -18,11 +18,34 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class SpeciesTests {
 
     @Autowired
+    private SpeciesRepository speciesRepository;
+
+    @Autowired
     private MockMvc mockMvc;
 
     @Test
-    @WithMockUser(username="admin", roles={"ADMIN"})
-    @PreAuthorize("authenticated")
+    public void Should_Throw_Unauthorized_Error() throws Exception {
+        mockMvc.perform(get("/api/species"))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @WithMockUser(username = "admin", roles = {"ADMIN"})
+    public void Should_Create_Species() throws Exception {
+        mockMvc.perform(post("/api/species/create").param("speciesName", "testSpecies"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser(username = "admin", roles = {"ADMIN"})
+    public void Should_Delete_Species() throws Exception {
+        int id = speciesRepository.findBySpeciesName("testSpecies").getId();
+        mockMvc.perform(delete("/api/species/{id}/delete", id))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @WithMockUser(username = "admin", roles = {"ADMIN"})
     public void Should_Get_All_Species() throws Exception {
         mockMvc.perform(get("/api/species/all"))
                 .andExpect(status().isOk())
