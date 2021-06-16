49054b0511a2d6ba98b4c23c54f8344e8a41dd5c
kamillobinski <kamil.lobinski@gmail.com>
2021-06-16 20:33:59 +0200

    Update: initial database data file

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -10,3 +10,7 @@ INSERT INTO ROLES (role_id, role) VALUES (1, 'ROLE_ADMIN'), (2, 'ROLE_USER')
 -- Create admin account
 INSERT INTO USER (avatar, username, first_name, last_name, email, password) VALUES ('user-avatar-default.jpg', 'admin', 'Admin', '', 'empty', '$2y$10$LrPlYkiJcKh./4rv3UNIbeXbEvXAB5hjyf.Du7Rh1MR8PIAa0.YGi')
 INSERT INTO USER_ROLE (user_id, role_id) VALUES (1, 1)
+-- Create default animal profile
+INSERT INTO ANIMAL (name, avatar, age, size_id, breed_id, gender_id, coat_length_id, house_trained_id, is_ready, comments) VALUES ('defaultAnimal', 'animal-avatar-default.jpg', 2, 1, 1, 1, 1, 1, 1, 'defaultText')
+INSERT INTO MEDICAL_DATA (description, animal_id) VALUES ('defaultText', 1)
+INSERT INTO MEDICAL_HISTORY (date, medicine, type, vet, animal_id, is_completed) VALUES ('2021-06-24', 'defaultMedicine', 'defaultType', 'defaultVet', 1, true), ('2021-06-10', 'defaultMedicine', 'defaultType', 'defaultVet', 1, false)
