d60d17de920034e19e48d775b4983735274445d0
kamillobinski <kamil.lobinski@gmail.com>
2021-06-17 12:01:36 +0200

    Update: initial database data file

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -14,6 +14,7 @@ INSERT INTO USER_ROLE (user_id, role_id) VALUES (1, 1)
 INSERT INTO ANIMAL (name, avatar, age, size_id, breed_id, gender_id, coat_length_id, house_trained_id, is_ready, comments) VALUES ('defaultAnimal', 'animal-avatar-default.jpg', 2, 1, 1, 1, 1, 1, 1, 'defaultText')
 INSERT INTO MEDICAL_DATA (description, animal_id) VALUES ('defaultText', 1)
 INSERT INTO MEDICAL_HISTORY (date, medicine, type, vet, animal_id, is_completed) VALUES ('2021-06-24', 'defaultMedicine', 'defaultType', 'defaultVet', 1, true), ('2021-06-10', 'defaultMedicine', 'defaultType', 'defaultVet', 1, false)
+INSERT INTO IMAGES (animal_id, url) VALUES (1, 'default_image.jpg')
 -- Create post
 INSERT INTO POST_CATEGORY (category) VALUE ('defaultCategory')
 INSERT INTO POST (date, text, title, author, category) VALUES ('2020-04-21', 'defaultText', 'defaultTitle', 1, 1)
